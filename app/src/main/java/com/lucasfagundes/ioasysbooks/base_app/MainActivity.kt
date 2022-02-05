580376a6effc02178abeb30bcd21a9eaf857bdd3
Lucas Fagundes <lukasfcarneiro@gmail.com>
2022-02-04 22:48:27 -0300

    splash and navigation created

---
+++ app/src/main/java/com/lucasfagundes/ioasysbooks/base_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.lucasfagundes.ioasysbooks.base_app
+
+import androidx.appcompat.app.AppCompatActivity
+import com.lucasfagundes.ioasysbooks.R
+
+class MainActivity : AppCompatActivity(R.layout.activity_main)
---
+++ app/src/main/java/com/lucasfagundes/ioasysbooks/base_app/SplashFragment.kt
@@ -0,0 +1,25 @@
+package com.lucasfagundes.ioasysbooks.base_app
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.fragment.app.Fragment
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import com.lucasfagundes.ioasysbooks.R
+
+class SplashFragment : Fragment(R.layout.fragment_splash) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Handler(Looper.getMainLooper())
+            .postDelayed(
+                {
+                    findNavController().navigate(
+                        SplashFragmentDirections.actionSplashFragmentToLoginFragment()
+                    )
+                }, 1000
+            )
+    }
+}
