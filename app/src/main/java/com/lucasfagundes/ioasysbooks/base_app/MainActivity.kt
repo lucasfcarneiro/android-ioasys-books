df86b2a75f4c73df6321c820b81a0598441b7ee5
Lucas Fagundes <lukasfcarneiro@gmail.com>
2022-02-18 00:16:52 -0300

    old splash screen fragment deleted

--- app/src/main/java/com/lucasfagundes/ioasysbooks/base_app/MainActivity.kt
+++ app/src/main/java/com/lucasfagundes/ioasysbooks/base_app/MainActivity.kt
@@ -5,15 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.lucasfagundes.ioasysbooks.R
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         installSplashScreen()
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 }
-
-
