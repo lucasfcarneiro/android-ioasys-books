91b9cbba4ab76e5528dd8cb0047b1fa9c1212614
Lucas Fagundes <lukasfcarneiro@gmail.com>
2022-02-17 22:08:00 -0300

    Splash screen library implemented

--- app/src/main/java/com/lucasfagundes/ioasysbooks/base_app/MainActivity.kt
+++ app/src/main/java/com/lucasfagundes/ioasysbooks/base_app/MainActivity.kt
@@ -1,7 +1,19 @@
 package com.lucasfagundes.ioasysbooks.base_app
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.lucasfagundes.ioasysbooks.R
 
-class MainActivity : AppCompatActivity(R.layout.activity_main)
+class MainActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        installSplashScreen()
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
 
